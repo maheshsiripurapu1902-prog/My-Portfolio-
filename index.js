410d34602aa74d62610dc3158480cea2269ebda7
zayn <siripurapumahesh7670@gmail.com>
2025-11-04 14:50:52 +0530

    index.js page

---
+++ index.js
@@ -0,0 +1,19 @@
+
+function BlackColor(){
+
+    document.getElementById("navbox").style.background="black";
+
+    document.getElementById("text-change").innerText="Dark Mode"
+}
+
+function  whiteCOlor(){
+    
+    document.getElementById("navbox").style.background="green";
+
+    document.getElementById("text-change").innerText="Green Mode"
+}
+
+function PrintMYResume(){
+    window.print();
+}
+
